1205db124b3d725f89a37c7007bf31c21df6743a
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-17 10:19:40 +0100

    Connected slider functionality

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func sliderDragged(_ sender: UISlider) {
+        let updatedValue = sender.value
+        
+        if sender == heightSlider {
+            heightValueLabel.text = "\(round(updatedValue * 100) / 100)m"
+        } else {
+            weightValueLabel.text = "\(Int(updatedValue))kg"
+        }
+    }
 }
 
